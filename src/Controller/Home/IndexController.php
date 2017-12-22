adf0d2a6877372367a1cf9d140e3ae68f369bc4f
coffey <coffey@nligo.com>
2017-12-22 16:54:09 +0800

    增加静态文件

--- src/Controller/Home/IndexController.php
+++ src/Controller/Home/IndexController.php
@@ -6,5 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class IndexController extends Controller
 {
+    public function index()
+    {
+        return $this->render('admin/index.html.twig', [ 'path' => str_replace($this->getParameter('kernel.project_dir').'/', '', __FILE__) ]);
+    }
 
 }
