211e7d28f9b77dcac41ee6d14f455fdd4ebb0d8a
高飞 <coffey@nligo.com>
2018-01-23 00:02:16 +0800

    增加邮件发发送日志

--- src/Controller/Home/IndexController.php
+++ src/Controller/Home/IndexController.php
@@ -2,12 +2,20 @@
 
 namespace App\Controller\Home;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class IndexController extends Controller
 {
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function index()
     {
+        $this->logger->critical("test");
         return $this->render('admin/index.html.twig', [ 'path' => str_replace($this->getParameter('kernel.project_dir').'/', '', __FILE__) ]);
     }
 
